55c7ea20d136e32a6de81cc5acc203766b4d7eae
whowho0820 <whowho0820@naver.com>
2020-08-05 03:18:59 -0400

    aaaa

--- src/main/java/com/yi/controller/AdminBoardMgrControoler.java
+++ src/main/java/com/yi/controller/AdminBoardMgrControoler.java
@@ -16,7 +16,7 @@ import com.yi.domain.SearchCriteria;
 import com.yi.service.BoardService;
 import com.yi.service.CafeService;
 // 수정함.
-//두번재수정.ddddd
+//두번재수정.
 @Controller
 @RequestMapping("/admin/boardMgr/*")
 public class AdminBoardMgrControoler {
